2c8ecd5825a114be3aad382a4a7fee137cf13ac8
juanl <garciaguirreg14gmail.com>
2023-02-13 12:20:52 -0600

    Guardar todos los cambios

--- app/src/main/java/com/example/conversordeunidades/MainActivity.java
+++ app/src/main/java/com/example/conversordeunidades/MainActivity.java
@@ -1,18 +1,64 @@
 package com.example.conversordeunidades;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
-
-
+    Button btmtd, btmmon, btmmasa, btmlongi, btmvolu, btmtiempo, btmalma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btmmon= findViewById(R.id.btmmon);
+        btmmon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),moneda.class));
 
+            }
+        });
+        btmvolu= findViewById(R.id.btmvolu);
+        btmvolu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),volumen.class));
+            }
+        });
+
+        btmalma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btmlongi= findViewById(R.id.btmlongi);
+        btmlongi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),longitud.class));
+            }
+        });
+        btmmasa= findViewById(R.id.btmmasa);
+        btmmasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btmtiempo= findViewById(R.id.btmtiempo);
+        btmtiempo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),tiempo.class));
+            }
+        });
+
+        }
 
     }
-}
+
+
+
